d6cba7b8c807683af61e77212e1452d100391b0b
Mark Kromis <mkromis@outlook.com>
2020-05-23 23:26:22 -0400

    add minori

--- src/VisualBasicUpgradeAssistant.Core/ViewModels/MainViewModel.cs
+++ src/VisualBasicUpgradeAssistant.Core/ViewModels/MainViewModel.cs
@@ -1,13 +1,17 @@
+using MvvmCross.Logging;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 
 namespace VisualBasicUpgradeAssistant.Core.ViewModels
 {
-    public class MainViewModel : MvxViewModel
+    public class MainViewModel : MvxNavigationViewModel
     {
         private readonly IMvxNavigationService _navigationService;
 
-        public MainViewModel(IMvxNavigationService navigationService)
+        public MainViewModel(
+            IMvxLogProvider logProvider,
+            IMvxNavigationService navigationService
+            ) : base(logProvider, navigationService)
         {
             _navigationService = navigationService;
         }
