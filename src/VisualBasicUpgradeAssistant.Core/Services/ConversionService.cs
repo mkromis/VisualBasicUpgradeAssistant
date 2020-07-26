79c29be03956ea55a3877607c3652e7f13e9e6f1
Mark Kromis <mkromis@outlook.com>
2020-07-26 18:28:46 -0400

    Create view for batch conversion

---
+++ src/VisualBasicUpgradeAssistant.Core/Services/ConversionService.cs
@@ -0,0 +1,12 @@
+using System.IO;
+
+namespace VisualBasicUpgradeAssistant.Core.Services
+{
+    internal class ConversionService
+    {
+        public void Convert(FileInfo source, DirectoryInfo dest)
+        {
+            DirectoryInfo sourceDir = source.Directory;
+        }
+    }
+}
